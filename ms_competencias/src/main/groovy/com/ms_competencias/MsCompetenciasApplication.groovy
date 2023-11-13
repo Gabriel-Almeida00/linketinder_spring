e202c85ac2eb3c2387342d6c0656325abc4f9780
Gabriel <gaamdal557@gmail.com>
2023-11-12 23:58:09 -0300

    first commit

---
+++ ms_competencias/src/main/groovy/com/ms_competencias/MsCompetenciasApplication.groovy
@@ -0,0 +1,13 @@
+package com.ms_competencias
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MsCompetenciasApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MsCompetenciasApplication, args)
+	}
+
+}
---
+++ ms_nucleo/src/main/groovy/com/ms_nucleo/MsNucleoApplication.groovy
@@ -0,0 +1,13 @@
+package com.ms_nucleo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MsNucleoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MsNucleoApplication, args)
+	}
+
+}
